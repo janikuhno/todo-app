d4aec34b70d2ece8065bcf1b66e03e89ae476451
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-19 15:18:47 +0200

    complete crud

--- server/index.js
+++ server/index.js
@@ -16,19 +16,61 @@ app.post('/todos', async (req, res) => {
   try {
     const { description } = req.body;
     const newTodo = await pool.query(
-      'INSERT INTO todo (description) VALUES($1)',
+      `INSERT INTO todo(description) VALUES($1) RETURNING *`,
       [description]
     );
 
-    res.json(newTodo);
+    res.json(newTodo.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
 });
 // get all todos
+app.get('/todos', async (req, res) => {
+  try {
+    const allTodos = await pool.query('SELECT * FROM todo');
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // get a todo
+app.get('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query('SELECT * FROM todo WHERE id = $1', [id]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // update a todo
+app.put('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      'UPDATE todo SET description = $1 WHERE id = $2 RETURNING *',
+      [description, id]
+    );
+    res.json(updateTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 // delete a todo
+app.delete('/todos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query(
+      'DELETE FROM todo WHERE id = $1 RETURNING *',
+      [id]
+    );
+    res.json(deleteTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
