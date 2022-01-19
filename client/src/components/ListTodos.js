23e761d1f7e50248a5b77c5da2d62b03c095c181
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-19 16:57:13 +0200

    added list of todos

---
+++ client/src/components/ListTodos.js
@@ -0,0 +1,45 @@
+import React, { Fragment, useEffect, useState } from 'react';
+
+const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    try {
+      const response = await fetch('http://localhost:5001/todos');
+      const jsonData = await response.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
+  return (
+    <Fragment>
+      <table className="table mt-5 text-center">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo) => (
+            <tr key={todo.id}>
+              <td>{todo.description}</td>
+              <td>Edit</td>
+              <td>Delete</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+export default ListTodos;
