1269052d286f4480cafaeee62df9454a8bc30446
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-20 18:46:46 +0200

    adjusted structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const pool = require('./db');
 
 // port number either defined or from env
 const PORT = 5001;
@@ -9,68 +8,6 @@ const PORT = 5001;
 // middleware
 app.use(cors());
 app.use(express.json());
-
-// routes
-// create a todo
-app.post('/todos', async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      `INSERT INTO todos(description) VALUES($1) RETURNING *`,
-      [description]
-    );
-
-    res.json(newTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-// get all todos
-app.get('/todos', async (req, res) => {
-  try {
-    const allTodos = await pool.query('SELECT * FROM todos');
-    res.json(allTodos.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-// get a todo
-app.get('/todos/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await pool.query('SELECT * FROM todos WHERE id = $1', [id]);
-    res.json(todo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-// update a todo
-app.put('/todos/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateTodo = await pool.query(
-      'UPDATE todos SET description = $1 WHERE id = $2 RETURNING *',
-      [description, id]
-    );
-    res.json(updateTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-// delete a todo
-app.delete('/todos/:id', async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query(
-      'DELETE FROM todos WHERE id = $1 RETURNING *',
-      [id]
-    );
-    res.json(deleteTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
