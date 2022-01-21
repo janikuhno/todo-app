1387ab85dee5381ef67fcea700f9abe5348610da
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-21 16:48:44 +0200

    fixes for deployment

--- db.js
+++ db.js
@@ -5,11 +5,10 @@ require('dotenv').config();
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 // production
-const proConfig = process.env.DATABASE_URL;
+const proConfig = { connectionString: process.env.DATABASE_URL };
 
-const pool = new Pool({
-  connectionString:
-    process.env.NODE_ENV === 'production' ? proConfig : devConfig,
-});
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? proConfig : devConfig
+);
 
 module.exports = pool;
