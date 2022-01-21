798a2a0d61709c7d43d64bdb9ff699d11eea790b
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-21 15:29:32 +0200

    bug fix

--- db.js
+++ db.js
@@ -2,22 +2,14 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 // development
-const devConfig = {
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-};
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 // production
-const prodConfig = {
-  connectionString: process.env.DATABASE_URL, // heroku addon
-};
+const proConfig = process.env.DATABASE_URL;
 
 const pool = new Pool({
   connectionString:
-    process.env.NODE_ENV === 'production' ? prodConfig : devConfig,
+    process.env.NODE_ENV === "production" ? proConfig : devConfig,
 });
 
 module.exports = pool;
