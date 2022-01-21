256bfd4386f242479dc7698ad43979087275eead
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-21 17:59:00 +0200

    bug fixing

--- db.js
+++ db.js
@@ -10,6 +10,9 @@ const proConfig = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
